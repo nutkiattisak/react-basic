1e4f10925c6e6998583bc97a3ef8935b904abf6d
kiattisak <kiattisak@diversition.co.th>
2022-04-16 22:27:13 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="">
+        <h1 className="text-4xl">React basic</h1>
       </header>
+      <div class="container mt-4 mx-auto">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div class="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <div class="m-3">
+              <h2 class="text-lg mb-2">React Hook</h2>
+              <p class="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200"></p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
