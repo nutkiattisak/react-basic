bff435053ea4ede760db6539d8659379dd77046f
kiattisak <kiattisak@diversition.co.th>
2022-04-24 23:24:06 +0700

    Update table of contents

--- src/page/react-hook/List.js
+++ src/page/react-hook/List.js
@@ -7,7 +7,31 @@ const ListReactHook = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <div className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
             <div className="m-3">
+              <h2 className="text-lg mb-2">useState</h2>
+              <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200"></p>
+            </div>
+          </div>
+          <div className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <div className="m-3">
               <h2 className="text-lg mb-2">useEffect</h2>
+              <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200"></p>
+            </div>
+          </div>
+          <div className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <div className="m-3">
+              <h2 className="text-lg mb-2">useContext</h2>
+              <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200"></p>
+            </div>
+          </div>
+          <div className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <div className="m-3">
+              <h2 className="text-lg mb-2">useRef</h2>
+              <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200"></p>
+            </div>
+          </div>
+          <div className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+            <div className="m-3">
+              <h2 className="text-lg mb-2">useReducer</h2>
               <p className="font-light font-mono text-sm text-gray-700 hover:text-gray-900 transition-all duration-200"></p>
             </div>
           </div>
