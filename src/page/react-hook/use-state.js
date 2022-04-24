99d45bc2a159e7cbffb47d57a084c7d012e1fb51
kiattisak <kiattisak.ch1995@gmail.com>
2022-04-24 23:55:39 +0700

    Update use-state.js

--- src/page/react-hook/use-state.js
+++ src/page/react-hook/use-state.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const useStatePage = () => {
+  const [count, setCount] = useState(0)
   return (
     <>
       <div className="container mt-4 mx-auto">
-        <h2 className="text-lg mb-2">useState</h2>
+        <h2 className="text-xl mb-2">useState</h2>
+
+        <button
+          type="button"
+          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count = {count}
+        </button>
       </div>
     </>
   )
